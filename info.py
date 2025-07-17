1a83fa3ba6277e8d160d529bf080b33beb9b3de9
Mimamofficial <mrmuzaffarimam@gmail.com>
2025-07-17 17:05:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002338765286'))  # Log channel i
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1002338765286'))  # Bin channel id (make sure bot is admin)
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002338765286'))  # Premium logs channel id
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002445547306').split()] #(make sure bot is admin)
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '1001837163489')  # Support group id (make sure bot is admin)
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1001837163489')  # Support group id (make sure bot is admin)
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002745555397')  # Request channel id (make sure bot is admin)
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Tv_Serial_Search_Group')  # Support group link (make sure bot is admin)
 
